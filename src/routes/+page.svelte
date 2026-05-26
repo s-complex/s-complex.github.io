fe78c5166b14c5e05cecf6792dd8332ae5f8c187
Restent Ou <i@gxres.net>
2026-05-27 00:15:40 +0800

    fix: update SliverRiver username

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <div class="text-center">
 	<h2>Welcome to Sliver Complex!</h2>
 	<p>
-		This is a lab of <a href="https://github.com/gxres042" target="_blank" rel="noopener noreferer"
+		This is a lab of <a href="https://github.com/gxres042" target="_blank" rel="noopener noreferrer"
 			>@gxres042</a
 		>
 		and
-		<a href="https://github.com/slivermoe" target="_blank" rel="noopener noreferer">@slivermoe</a>
+		<a href="https://github.com/inslicc" target="_blank" rel="noopener noreferrer">@inslicc</a>
 	</p>
 </div>
